94b740eccf636ae32d3a2d0ade6701b8412e1f1c
cesarf <ccflores567@gmail.com>
2026-04-24 19:53:31 -0300

    feat: add jdtls to lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -210,6 +210,7 @@ return {
 			pyright = {},
 			black = {},
 			isort = {},
+			jdtls = {},
 			-- rust_analyzer = {},
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 			--
