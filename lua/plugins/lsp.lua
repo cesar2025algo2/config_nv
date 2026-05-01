7710cdd2ef60f7d892b58f4bfc4cd6d129f0c481
cesarf <ccflores567@gmail.com>
2026-04-30 23:17:21 -0300

    feat: add texlab to lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -220,7 +220,19 @@ return {
 			-- But for many setups, the LSP (`ts_ls`) will work just fine
 			-- ts_ls = {},
 			--
-
+			texlab = {
+				-- settings = {
+				-- 	texlab = {
+				-- 		build = {
+				-- 			onSave = true,
+				-- 		},
+				-- 		chktex = {
+				-- 			onEdit = true,
+				-- 			onOpen = true,
+				-- 		},
+				-- 	},
+				-- },
+			},
 			lua_ls = {
 				-- cmd = { ... },
 				-- filetypes = { ... },
